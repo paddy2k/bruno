4a7e652d5f85e3f98e1fb600e3302b984e1af61f
Paddy O'Reilly <p@ddy.space>
2018-05-29 23:22:45 +0100

    Added MariaDB

--- server.js
+++ server.js
@@ -5,66 +5,87 @@ const ident_api = "https://iam-api.dss.husqvarnagroup.net/api/v3/token";
 const main_api = "https://amc-api.dss.husqvarnagroup.net/v1"
 
 let state = {};
+let con = mysql.createConnection({
+  host: "localhost",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
+});
 
 function getToken(){
-	request.post(ident_api, 
-		{	
-			json:true,
-			body: {
-				"data": {
-					"attributes": {
-						"username": process.env.HUSQUVARNA_USERNAME,
-						"password": process.env.HUSQUVARNA_PASSWORD,
-					},
-					"type": "token"
-				}
-			}
-		}, 
-		function(err, res, body) {
-			if (err) { return console.log(err); }
-			state['token'] = body.data.id;
-			state['provider'] = body.data.attributes.provider;
+  request.post(ident_api, 
+    { 
+      json:true,
+      body: {
+        "data": {
+          "attributes": {
+            "username": process.env.HUSQUVARNA_USERNAME,
+            "password": process.env.HUSQUVARNA_PASSWORD,
+          },
+          "type": "token"
+        }
+      }
+    }, 
+    function(err, res, body) {
+      if (err) { return console.log(err); }
+      state['token'] = body.data.id;
+      state['provider'] = body.data.attributes.provider;
 
-			getMowers();
-		}
-	)
+      getMowers();
+    }
+  )
 }
 
 
 function hqRequest(url, callback){
-	request.get(
-		{
-			url: `${main_api}${url}`,
-			json:true,
-			headers: {
-				"Authorization": `Bearer ${state['token']}`,
-				"Authorization-Provider": state['provider'],
-			}
-		},
-		callback
-	)
+  request.get(
+    {
+      url: `${main_api}${url}`,
+      json:true,
+      headers: {
+        "Authorization": `Bearer ${state['token']}`,
+        "Authorization-Provider": state['provider'],
+      }
+    },
+    callback
+  )
 }
 
 function getMowers(){
-	hqRequest("/mowers", function(err, res, body) {
-		if (err) { return console.log(err); }
-		state['mower_id'] = body[0].id;
-		
-		main();
-	})
+  hqRequest("/mowers", function(err, res, body) {
+    if (err) { return console.log(err); }
+    state['mower_id'] = body[0].id;
+    
+    main();
+  })
 }
 
 function getMowerStatus(callback){
-	hqRequest(`/mowers/${state['mower_id']}/status`, function(err, res, body) {
-		if (err) { return console.log(err); }
-		callback(body);
-	})
+  hqRequest(`/mowers/${state['mower_id']}/status`, function(err, res, body) {
+    if (err) { return console.log(err); }
+    callback(body);
+  })
 }
 
-getToken();
+
+function init(){
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Maria DB Connected!");
+    getToken();
+  });
+}
 
 function main(){
-	getMowerStatus(function(body){
-		console.log(body)
-	});
+  getMowerStatus(function(body){
+    console.log({
+      "storedTimestamp": body.storedTimestamp,
+      "batteryPercent": body.batteryPercent,
+      "mowerStatus": body.mowerStatus,
+      "operatingMode": body.operatingMode,
+      "latitude": body.lastLocations[0].latitude,
+      "longitude": body.lastLocations[0].longitude,
+    })
+  });
 }
+
+init();
