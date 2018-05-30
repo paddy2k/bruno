910e88f85b29ba151f3f6f58e7cc96dd157b2bf2
Paddy O'Reilly <p@ddy.space>
2018-05-30 23:20:06 +0100

    Updated API

--- api.js
+++ api.js
@@ -1,13 +1,37 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
+const dotenv = require("dotenv").config();
+const mysqlClient = require("mysql");
+const express = require("express");
 const app = express();
 
+let mysql = mysqlClient.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+mysql.connect(function(err) {
+  if (err) throw err;
+  console.log("Maria DB Connected!")
+});
+
 app
-  .get('/api', (req, res) => {
-    res.send('Hello World!') 
+  .get("/api", (req, res) => {
+    res.send("Hello World!") 
   })
-  .get('/api/mowers', (req, res) => {
-    res.send('Hello MOWERS!') 
+  .get("/api/mowers", (req, res) => {
+  	mysql.query("SELECT DISTINCT mower_id FROM mower_log ORDER BY mowerStatus ASC", function (error, results, fields) {
+			const mowers = results.map((result) => result['mower_id']);
+  		res.send(mowers);
+  	})
+  })
+  .get("/api/mowers/:mowerId", (req, res) => {
+  	const mower_id = req.params.mowerId;
+
+  	mysql.query("SELECT * FROM mower_log WHERE mower_id=" + mysql.escape(mower_id), function (error, results, fields) {
+  		res.send(results);
+  	})
   })
 ;
+
 app.listen(process.env.SERVER_PORT, () => console.log(`Example app listening on port ${process.env.SERVER_PORT}!`))
